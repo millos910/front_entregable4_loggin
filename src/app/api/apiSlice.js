f23b8ac983f47b2477b95c50786daad9f6c792ce
millos910 <emontero910@gmail.com>
2023-08-09 04:14:14 -0500

    link render

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 const baseQuery = fetchBaseQuery({
-    //baseUrl: process.env.REACT_APP_API_URL,
-    baseUrl:'https://auth-db-entregable4.onrender.com/api/v1', 
+    baseUrl: process.env.REACT_APP_API_URL,
     prepareHeaders: (headers, { getState }) => {
         const token = getState().localStorage.token;
         if(token){
